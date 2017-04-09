c798b28d7bb03c812a4bed0c58839612a523d527
hvad <david.hannequin@gmail.com>
2017-04-09 18:55:59 +0200

    use recv and send

--- tcp/client.c
+++ tcp/client.c
@@ -8,34 +8,30 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-#define PORTS 60000 /* port du serveur */
-#define IP_S "192.168.122.91" /* adresse ip du serveur */
+#define PORTS 60000		/* port du serveur */
+#define IP_S "192.168.122.91"	/* adresse ip du serveur */
 
-int main() 
+int
+main ()
 {
-  struct hostent *h; /* TODO */
-  struct sockaddr_in adr_s,adr_c; 
+  struct sockaddr_in adr_s, adr_c;
   char buf[20];
   int sock, a;
 
-  if((sock = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP)) < 0)
-  {
-    perror("socket");
-    exit(1);
-  }
+  if ((sock = socket (AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+    {
+      perror ("socket");
+      exit (1);
+    }
 
   adr_s.sin_family = AF_INET;
-  adr_s.sin_port = htons(PORTS); /* passage au format réseau */
-  adr_s.sin_addr.s_addr = inet_addr(IP_S); /* adresse IP de la machine */
+  adr_s.sin_port = htons (PORTS);	/* passage au format réseau */
+  adr_s.sin_addr.s_addr = inet_addr (IP_S);	/* adresse IP de la machine */
 
-  a = connect(sock, (struct sockaddr*) &adr_s, sizeof(adr_s));
+  a = connect (sock, (struct sockaddr *) &adr_s, sizeof (adr_s));
 
-//  if(write(sock, "bonjour, serveur !!", 20) < 0)
-  if(write(a, "bonjour, serveur !!\n", 20) < 0)
-    {
-      printf("message not send"); 
-    }
-  
-  close(sock);    
+  send (sock, "bonjour, serveur !! ", 20, 0);
+
+  close (sock);
 
 }
--- tcp/serveur.c
+++ tcp/serveur.c
@@ -8,10 +8,11 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-#define PORTS 60000 /* port du serveur */
+#define PORTS 60000		/* port du serveur */
 #define TRUE 1
 
-int main()
+int
+main ()
 {
   int namelen, sock, nsock;
   char buf[20];
@@ -22,46 +23,46 @@ int main()
 
 
   /* Création socket */
-  if ((sock = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP)) < 0)
-  {
-    perror("socket");
-    exit(errno);
-  }
+  if ((sock = socket (AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+    {
+      perror ("socket");
+      exit (errno);
+    }
 
   /* Attachement socket */
 
   adr_s.sin_family = AF_INET;
-  adr_s.sin_port = htons(PORTS); /* passage au format réseau */
-  adr_s.sin_addr.s_addr = htonl(INADDR_ANY); /* n'importe quelle adresse IP de la machine locale */
+  adr_s.sin_port = htons (PORTS);	/* passage au format réseau */
+  adr_s.sin_addr.s_addr = htonl (INADDR_ANY);	/* n'importe quelle adresse IP de la machine locale */
 
-  if(bind(sock, (struct sockaddr*) &adr_s, sizeof(adr_s)) < 0)
-  {
-    perror("bind");
-    exit(2);
-  }
-    
-  if(listen(sock,5) < 0)
-  {
-    perror("listen");
-    exit(4);
-  }
-  
+  if (bind (sock, (struct sockaddr *) &adr_s, sizeof (adr_s)) < 0)
+    {
+      perror ("bind");
+      exit (2);
+    }
+
+  if (listen (sock, 5) < 0)
+    {
+      perror ("listen");
+      exit (4);
+    }
+
   /* Boucle d'acceptation d'une connexion */
 
-  while(TRUE) 
-  {
-    lg = sizeof (adr_c);
-    nsock = accept (sock,(struct sockaddr*)  &adr_c, &lg);
-    pid = fork();
-
-    if (pid == 0)
+  while (TRUE)
     {
-      close (sock);
-      n = read (nsock, buf, 20);
-      printf("affiche %d", n);
-      close (nsock);
-      exit(0);
+      lg = sizeof (adr_c);
+      nsock = accept (sock, (struct sockaddr *) &adr_c, &lg);
+      pid = fork ();
+
+      if (pid == 0)
+	{
+	  close (sock);
+	  n = recv (nsock, buf, 20, 0);
+	  printf ("Chaine reçue %s\n", buf);
+	  close (nsock);
+	  exit (0);
+	}
     }
-  }    
   return 0;
 }
