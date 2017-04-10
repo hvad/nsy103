fe00c4584852d52d30a06c216c4e053d18beb0b7
hvad <david.hannequin@gmail.com>
2017-04-10 20:46:37 +0200

    unamed pipe version 0.2

--- pipe/unamed.c
+++ pipe/unamed.c
@@ -1,10 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <unistd.h>    
 
-int main(void)
+int pip[2];
+char buf [40];
+
+int pere()
+{
+    write (pip[1],"hello, je suis le processus A",40);
+    exit(0);
+}
+
+int fils()
+{
+  read (pip[0],buf,40);
+  printf("%s", buf);
+  exit(0);
+}
+
+int main (void)
 {
 
-	printf("Hello World!\n");
-	return 0;
+  pipe(pip);
+  int f;
+  
+  f = fork();
+  
+  printf("%d\n",f);
+
+  switch(f)
+  {
+    case -1: perror("pipe");
+             exit(1);
+    case 0: fils();
+    default: pere();         
+
+  }
+
+  return 0;
 
 }
